9763b41b17d9e5af42303f15921fa4572e8f0e79
Abhishek Vankar <abhishekv@advic.io>
2026-03-26 16:27:22 +0530

    upated the version

--- src-tauri/preload.js
+++ src-tauri/preload.js
@@ -172,53 +172,24 @@
         if (url.includes('/v2/trackManifests/')) {
             try {
                 const parsedUrl = new URL(url);
-                const id = parsedUrl.pathname.split("/").pop();
-                const formats = parsedUrl.searchParams.getAll('formats');
 
-                let quality = "LOW";
-                if (formats.includes('FLAC_HIRES')) quality = "HI_RES_LOSSLESS";
-                else if (formats.includes('FLAC')) quality = "LOSSLESS";
-                else if (formats.includes('AACLC')) quality = "HIGH";
-                else if (formats.includes('HEAACV1')) quality = "LOW";
+                // Extract track ID
+                const pathParts = parsedUrl.pathname.split('/');
+                const id = pathParts[pathParts.length - 1];
 
-                console.log("TrackManifest intercept:", id, "Quality:", quality);
+                // Keep all original query params
+                const query = parsedUrl.searchParams.toString();
 
+                // Build new URL
+                const newUrl = `${localStorage.getItem('streamurl')}/trackManifests/?id=${id}&${query}`;
                 const requestOptions = {
                     method: "GET",
                     redirect: "follow"
                 };
 
-                const monoResponse = await originalFetch.call(globalThis, localStorage.getItem('streamurl') + "/track/?id=" + id + "&quality=" + quality, requestOptions);
-                const y = await monoResponse.json();
-
-                const data = {
-                    "data": {
-                        "id": y.data.trackId,
-                        "type": "trackManifests",
-                        "attributes": {
-                            "trackPresentation": "Full",
-                            // "previewReason": "FULL_REQUIRES_SUBSCRIPTION",
-                            "uri": "data:" + y.data.manifestMimeType + ";base64," + y.data.manifest,
-                            "hash": y.data.manifestHash,
-                            "formats": formats,
-                            "albumAudioNormalizationData": {
-                                "replayGain": y.data.albumReplayGain,
-                                "peakAmplitude": y.data.albumPeakAmplitude
-                            },
-                            "trackAudioNormalizationData": {
-                                "replayGain": y.data.trackReplayGain,
-                                "peakAmplitude": y.data.trackPeakAmplitude
-                            }
-                        }
-                    },
-                    "links": {
-                        "self": `/trackManifests/${id}?uriScheme=DATA&adaptive=true&formats=${encodeURIComponent(
-                            formats.join(",")
-                        )}&usage=PLAYBACK&manifestType=MPEG_DASH`
-                    }
-                }
-
-                return new Response(JSON.stringify(data), {
+                const monoResponse = await originalFetch.call(globalThis, newUrl, requestOptions);
+                const data = await monoResponse.json();
+                return new Response(JSON.stringify(data.data), {
                     status: monoResponse.status,
                     statusText: monoResponse.statusText,
                     headers: monoResponse.headers
